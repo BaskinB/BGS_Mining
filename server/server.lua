03bca03454ac7460c708c57102bef09ddd388ed9
Snapopotamus <spoono17@gmail.com>
2024-09-18 07:28:19 -0600

    fixed issue with no dropped items on mining node throwing error

--- server/server.lua
+++ server/server.lua
@@ -20,9 +20,9 @@ AddEventHandler("BGS_Mining:pickaxecheck", function(metadata)
 		end
 		if meta.durability == nil then
 			local durability = 100 - Config.PickaxeDamage
-			VorpInv.subItem(_source, Config.Pickaxe, 1,{})
-			VorpInv.addItem(_source, Config.Pickaxe, 1,{description = "Durability = "..durability,durability = durability})
-			TriggerClientEvent("BGS_Mining:pickaxechecked", _source, {description = "Durability = "..durability,durability = durability}, false)
+			VorpInv.subItem(_source, Config.Pickaxe, 1, {})
+			VorpInv.addItem(_source, Config.Pickaxe, 1, { description = "Durability = " .. durability, durability = durability })
+			TriggerClientEvent("BGS_Mining:pickaxechecked", _source, { description = "Durability = " .. durability, durability = durability }, false)
 		else
 			local durability = meta.durability - Config.PickaxeDamage
 			local description = "Durability = "
@@ -30,8 +30,8 @@ AddEventHandler("BGS_Mining:pickaxecheck", function(metadata)
 			if 1 > durability then
 				TriggerClientEvent("BGS_Mining:pickaxechecked", _source, meta, true)
 			else
-				VorpInv.addItem(_source, Config.Pickaxe, 1,{description = description..durability,durability = durability})
-				TriggerClientEvent("BGS_Mining:pickaxechecked", _source, {description = description..durability,durability = durability}, false)
+				VorpInv.addItem(_source, Config.Pickaxe, 1, { description = description .. durability, durability = durability })
+				TriggerClientEvent("BGS_Mining:pickaxechecked", _source, { description = description .. durability, durability = durability }, false)
 			end
 		end
 	end
@@ -49,11 +49,11 @@ AddEventHandler("BGS_Mining:CountCheck", function()
 end)
 
 local function keysx(table)
-    local keys = 0
-    for k,v in pairs(table) do
-       keys = keys + 1
-    end
-    return keys
+	local keys = 0
+	for k, v in pairs(table) do
+		keys = keys + 1
+	end
+	return keys
 end
 
 local function shuffle(tbl)
@@ -67,24 +67,26 @@ end
 RegisterServerEvent('BGS_Mining:addItem')
 AddEventHandler('BGS_Mining:addItem', function(mineSpot)
 	local _source = source
-	local chance =  math.random(1,10)
+	local chance = math.random(1, 10)
 	local itemTable = shuffle(mineSpot.items)
 	local reward = {}
-	for k,v in pairs(itemTable) do
+	for k, v in pairs(itemTable) do
 		if v.chance >= chance then
-			table.insert(reward,v)
+			table.insert(reward, v)
 		end
 	end
-	local chance2 = math.random(1,keysx(reward))
-	local count = math.random(1,reward[chance2].amount)
-	TriggerEvent("vorpCore:canCarryItems", tonumber(_source), count, function(canCarry)
-		TriggerEvent("vorpCore:canCarryItem", tonumber(_source), reward[chance2].name,count, function(canCarry2)
-			if canCarry and canCarry2 then
-				VorpInv.addItem(_source, reward[chance2].name, count)
-				TriggerClientEvent("vorp:TipRight", _source, "You found "..reward[chance2].label, 3000)
-			else
-				TriggerClientEvent("vorp:TipRight", _source, "You can't carry any more "..reward[chance2].label, 3000)
-			end
+	if reward then
+		local chance2 = math.random(1, keysx(reward))
+		local count = math.random(1, reward[chance2].amount)
+		TriggerEvent("vorpCore:canCarryItems", tonumber(_source), count, function(canCarry)
+			TriggerEvent("vorpCore:canCarryItem", tonumber(_source), reward[chance2].name, count, function(canCarry2)
+				if canCarry and canCarry2 then
+					VorpInv.addItem(_source, reward[chance2].name, count)
+					TriggerClientEvent("vorp:TipRight", _source, "You found " .. reward[chance2].label, 3000)
+				else
+					TriggerClientEvent("vorp:TipRight", _source, "You can't carry any more " .. reward[chance2].label, 3000)
+				end
+			end)
 		end)
-	end) 
+	end
 end)
